9fabfbcc39d83240f01e93747c9fadc20d477bbc
joaquinrmi <joaquinruaimi@gmail.com>
2022-03-03 20:07:01 -0300

    Add Public page

---
+++ src/pages/public/index.tsx
@@ -0,0 +1,2 @@
+export { default } from "./public";
+export * from "./public";
---
+++ src/pages/public/public.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./public.scss";
+
+export interface Props
+{}
+
+const Public: React.FunctionComponent<Props> = (props) =>
+{
+    return <></>;
+};
+
+export default Public;
