e33582dd2a5cc7e0ae75090f3b5203fd6ddf6435
joaquinrmi <joaquinruaimi@gmail.com>
2022-03-03 20:32:28 -0300

    Add default route in App component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Public from "../pages/public/";
 
 import "./app.scss";
 
 const App: React.FunctionComponent = () =>
 {
-    return <>
-        Hola mundo
-    </>;
+    return <Routes>
+        <Route path="/*" element={<Public />} />
+    </Routes>
 };
 
 export default App;
