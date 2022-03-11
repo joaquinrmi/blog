12539fe1a3e7d2184d62a77292ba07e528ba1f5b
joaquinrmi <joaquinruaimi@gmail.com>
2022-03-11 15:37:02 -0300

    Add main header to Public page

--- src/pages/public/public.tsx
+++ src/pages/public/public.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./components/navbar";
 
 import "./public.scss";
 
@@ -7,7 +8,23 @@ export interface Props
 
 const Public: React.FunctionComponent<Props> = (props) =>
 {
-    return <></>;
+    return <>
+        <Navbar sections={[
+            {
+                name: "Inicio",
+                path: "/"
+            },
+            {
+                name: "Animales",
+                path: "/animals"
+            }
+        ]} currentSection="/" />
+
+        <header className="main-header">
+            <h1>Blog de ejemplo</h1>
+            <h2>Un blog sobre nada</h2>
+        </header>
+    </>;
 };
 
 export default Public;
