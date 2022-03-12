16526979efcb7c4182de22718c61eff648b5c88d
joaquinrmi <joaquinruaimi@gmail.com>
2022-03-12 09:08:03 -0300

    Add aside to Public page

--- src/pages/public/public.tsx
+++ src/pages/public/public.tsx
@@ -24,6 +24,30 @@ const Public: React.FunctionComponent<Props> = (props) =>
             <h1>Blog de ejemplo</h1>
             <h2>Un blog sobre nada</h2>
         </header>
+        
+        <section className="main-content">
+            <div className="main-body-container">
+                <aside className="main-left-section">
+                    <div className="about-author">
+                        <div className="photo-container">
+                            <img src="" alt="" />
+                        </div>
+
+                        <div className="author-information">
+                            <span className="name">
+                                Nombre Autor
+                            </span>
+                            <span className="bio">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. In expedita ducimus quia illum aspernatur, recusandae delectus, doloribus veritatis ullam placeat voluptas accusamus aliquam nisi? Laboriosam perferendis nisi asperiores laudantium eligendi!
+                            </span>
+                        </div>
+                    </div>
+                </aside>
+
+                <section className="main-right-section">
+                </section>
+            </div>
+        </section>
     </>;
 };
 
