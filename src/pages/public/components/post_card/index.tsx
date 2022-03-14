912d62b217d9c753bfacc501fae6d40095067117
joaquinrmi <joaquinruaimi@gmail.com>
2022-03-14 08:43:28 -0300

    Add PostCard component

---
+++ src/pages/public/components/post_card/index.tsx
@@ -0,0 +1,2 @@
+export { default } from "./post_card";
+export * from "./post_card";
---
+++ src/pages/public/components/post_card/post_card.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./post_card.scss";
+
+export interface Props
+{
+    data: PostCardData;
+}
+
+export interface PostCardData
+{
+    title: string;
+    cover: string;
+    tags: Array<{ name: string, path: string }>;
+    contentFragment: string;
+    url: string;
+}
+
+const PostCard: React.FunctionComponent<Props> = (props) =>
+{
+    return <Link className="post-card" to={props.data.url}>
+        <div className="card-post-cover" style={{
+            backgroundImage: `url(${props.data.cover})`
+        }}></div>
+
+        <div className="card-post-content">
+            <div className="tags">
+                {props.data.tags.map((tag, index) =>
+                {
+                    return <>
+                        <Link key={`tag-${index}`} to={`/category/${tag.path}`}>
+                            {tag.name}
+                        </Link>
+                        {index + 1 < props.data.tags.length ? <span className="comma">,</span> : null}
+                    </>;
+                })}
+            </div>
+            <h3 className="title">
+                {props.data.title}
+            </h3>
+            <span className="fragment">
+                {props.data.contentFragment}
+            </span>
+        </div>
+    </Link>;
+};
+
+export default PostCard;
