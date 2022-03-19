f6bc50cb79bc2871d8039eabb2736c223c8d6e84
joaquinrmi <joaquinruaimi@gmail.com>
2022-03-19 00:57:15 -0300

    Add loading animation to PopularPosts component

--- src/pages/public/components/popular_posts/popular_posts.tsx
+++ src/pages/public/components/popular_posts/popular_posts.tsx
@@ -13,80 +13,117 @@ interface PopularPostData
 
 const PopularPosts: React.FunctionComponent = () =>
 {
-    const [ data, setData ] = useState<Array<PopularPostData>>([
+    const [ data, setData ] = useState<Array<PopularPostData>>([]);
+
+    useEffect(() =>
+    {
+        if(data.length > 0)
         {
-            id: "hola",
-            title: "Quiero ver cómo queda un título largo",
-            tags: [ { name: "Animales", path: "animals" } ],
-            date: new Date()
-        },
-        {
-            id: "hola",
-            title: "Quiero ver cómo queda un título largo",
-            tags: [
-                { name: "Animales", path: "animals" },
-                { name: "Ciencia", path: "science" }
-            ],
-            date: new Date()
-        },
-        {
-            id: "hola",
-            title: "Quiero ver cómo queda un título largo",
-            tags: [
-                { name: "Animales", path: "animals" },
-                { name: "Ciencia", path: "science" }
-            ],
-            date: new Date()
-        },
-        {
-            id: "hola",
-            title: "Quiero ver cómo queda un título largo",
-            tags: [
-                { name: "Animales", path: "animals" },
-                { name: "Ciencia", path: "science" }
-            ],
-            date: new Date()
-        },
-        {
-            id: "hola",
-            title: "Quiero ver cómo queda un título largo",
-            tags: [
-                { name: "Animales", path: "animals" },
-                { name: "Ciencia", path: "science" }
-            ],
-            date: new Date()
+            return;
         }
-    ]);
+
+        setTimeout(() =>
+        {
+            setData([
+                {
+                    id: "hola",
+                    title: "Lorem ipsum dolor sit amet",
+                    tags: [
+                        { name: "Animales", path: "animals" },
+                        { name: "Ciencia", path: "science" }
+                    ],
+                    date: new Date()
+                },
+                {
+                    id: "hola",
+                    title: "Lorem ipsum dolor sit amet",
+                    tags: [
+                        { name: "Animales", path: "animals" },
+                        { name: "Ciencia", path: "science" }
+                    ],
+                    date: new Date()
+                },
+                {
+                    id: "hola",
+                    title: "Lorem ipsum dolor sit amet",
+                    tags: [
+                        { name: "Animales", path: "animals" },
+                        { name: "Ciencia", path: "science" }
+                    ],
+                    date: new Date()
+                },
+                {
+                    id: "hola",
+                    title: "Lorem ipsum dolor sit amet",
+                    tags: [
+                        { name: "Animales", path: "animals" },
+                        { name: "Ciencia", path: "science" }
+                    ],
+                    date: new Date()
+                },
+                {
+                    id: "hola",
+                    title: "Lorem ipsum dolor sit amet",
+                    tags: [
+                        { name: "Animales", path: "animals" },
+                        { name: "Ciencia", path: "science" }
+                    ],
+                    date: new Date()
+                }
+            ]);
+        },
+        3000);
+    });
+
+    let content;
+    if(data.length > 0)
+    {
+        content = <>{data.map((postData, index) =>
+        {
+            return <li key={`${index}-item`} className="popular-item">
+                <Link to={`/post/${postData.id}`} className={`popular-title s${index + 1}`}>
+                    <h4>
+                        {postData.title}
+                    </h4>
+                </Link>
+
+                <span className="popular-date">
+                    {`${postData.date.getDate()}/${postData.date.getMonth()}/${postData.date.getFullYear()}`}
+                </span>
+
+                <span className="popular-tags">
+                    {postData.tags.map((tag, index) =>
+                    {
+                        return <span key={`${index}-tag`} className="popular-tag">
+                            <Link to={`/category/${tag.path}`}>
+                                {tag.name}
+                            </Link>
+                            {index + 1 < postData.tags.length ? <span className="comma">,</span> : null}
+                        </span>;
+                    })}
+                </span>
+            </li>;
+        })}</>;
+    }
+    else
+    {
+        content = <>{[...new Array(5)].map((v, index) =>
+        {
+            return <li key={`${index}-litem`} className="popular-item popular-item-loading">
+                <div className={`post-loading-title s${index + 1} loading`}></div>
+                <div className="post-loading-date loading"></div>
+                <div className="post-loading-tags">
+                    <span className="loading"></span>
+                    <span className="loading"></span>
+                </div>
+            </li>;
+        })}</>;
+    }
 
     return <div className="popular-posts">
         <h3 className="title">Popular</h3>
         <ul className="popular-post-list">
-            {data.map((postData, index) =>
-            {
-                return <li key={`${index}-item`} className="popular-item">
-                    <Link to={`/post/${postData.id}`} className={`popular-title s${index + 1}`}>
-                        <h4>
-                            {postData.title}
-                        </h4>
-                    </Link>
-
-                    <span className="popular-date">
-                        {`${postData.date.getDate()}/${postData.date.getMonth()}/${postData.date.getFullYear()}`}
-                    </span>
-
-                    <span className="popular-tags">
-                        {postData.tags.map((tag, index) =>
-                        {
-                            return <span key={`${index}-tag`} className="popular-tag">
-                                <Link to={`/category/${tag.path}`}>
-                                    {tag.name}
-                                </Link>
-                                {index + 1 < postData.tags.length ? <span className="comma">,</span> : null}
-                            </span>;
-                        })}
-                    </span>
-                </li>;
-            })}
+            {content}
         </ul>
     </div>;
 };
