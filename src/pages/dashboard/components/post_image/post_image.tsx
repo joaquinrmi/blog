4f03d8d37276b402d863cf5364f7bc95f3b1389a
joaquinrmi <joaquinruaimi@gmail.com>
2022-04-01 20:56:00 -0300

    Add 'initSrc' to PostImage Props

--- src/pages/dashboard/components/post_image/post_image.tsx
+++ src/pages/dashboard/components/post_image/post_image.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PostElement from "../post_element/";
 import Imageinput from "../../../../components/image_input/";
 import uploadImage from "../../upload_image";
@@ -8,15 +8,18 @@ import "./post_image.scss";
 export interface Props
 {
     id: string;
+    initSrc?: string;
 
     erase(): void;
 }
 
 const PostImage: React.FunctionComponent<Props> = (props) =>
 {
+    const [ initSrc ] = useState<string | undefined>(props.initSrc);
+
     return <PostElement id={props.id} erase={props.erase}>
         <div className="new-post-image-input-container">
-            <Imageinput id={`${props.id}`} inputClassName="" imageContainerClassName="new-post-image-container" onChange={uploadImage} />
+            <Imageinput id={`${props.id}`} inputClassName="" imageContainerClassName="new-post-image-container" onChange={uploadImage} initSrc={initSrc} />
         </div>
     </PostElement>;
 };
