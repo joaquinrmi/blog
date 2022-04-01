305e201b53498ccf7e45ca56b33f58e1a4711802
joaquinrmi <joaquinruaimi@gmail.com>
2022-04-01 20:55:40 -0300

    Add 'value' to PostText Props

--- src/pages/dashboard/components/post_text/post_text.tsx
+++ src/pages/dashboard/components/post_text/post_text.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import PostElement from "../post_element/";
 
 import "./post_text.scss";
@@ -6,12 +6,15 @@ import "./post_text.scss";
 export interface Props
 {
     id: string;
+    value?: string;
 
     erase(): void;
 }
 
 const PostText: React.FunctionComponent<Props> = (props) =>
 {
+    const [ initValue ] = useState<string>(props.value ? props.value : "");
+
     useEffect(() =>
     {
         const textElement = document.getElementById(`textarea-${props.id}`) as HTMLTextAreaElement;
@@ -24,7 +27,7 @@ const PostText: React.FunctionComponent<Props> = (props) =>
     });
 
     return <PostElement id={props.id} erase={props.erase}>
-        <textarea placeholder="Comience a escribir aquí" id={`textarea-${props.id}`} className="new-post-textarea"></textarea>
+        <textarea placeholder="Comience a escribir aquí" id={`textarea-${props.id}`} className="new-post-textarea" value={initValue}></textarea>
     </PostElement>;
 };
 
